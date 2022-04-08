e89c666c8e136423b2a82b19257543535b1463c2
Fomin Artem <terra.integer@gmail.com>
2022-04-08 15:27:10 +0300

    Коррекция 5 задания

--- 2. Встроенные типы и операции с ними/2-5.py
+++ 2. Встроенные типы и операции с ними/2-5.py
@@ -6,6 +6,9 @@ rating = Queue(5)
 
 while True:
     number = utils.input.request_number("Please enter number\n")
+    if list(rating.queue).__len__() > 0 and number > list(rating.queue).pop():
+        print("Sequence should not decrease. Please try again.")
+        continue
     if rating.full():
         print(f"{rating.get()} dropped from rating")
     rating.put(number)
